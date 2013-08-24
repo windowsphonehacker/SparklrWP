b995c937cea694513f610510f9ed799931f5956e
jessenic <jessenic@digiex.net>
2013-08-24 21:45:46 +0300

    Fix last time

--- SparklrWP/ViewModels/MainViewModel.cs
+++ SparklrWP/ViewModels/MainViewModel.cs
@@ -15,6 +15,7 @@ using System.Collections.ObjectModel;
 using Newtonsoft.Json;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using System.Threading;
 
 
 namespace SparklrWP
@@ -58,23 +59,41 @@ namespace SparklrWP
             private set;
         }
 
+        public int LastTime = 1377357375;
+        private bool isInLoadCycle = false;
+
         /// <summary>
         /// Creates and adds a few ItemViewModel objects into the Items collection.
         /// </summary>
         public void LoadData()
         {
-            App.Client.BeginRequest(loadCallback,
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    while (isInLoadCycle)
+                    {
+                    }
+                    isInLoadCycle = true;
+                    App.Client.BeginRequest(loadCallback,
 #if DEBUG
- "beacon/stream/2?since=1377357375&n=0&network=1" //Development network
+ "beacon/stream/2?since=" + LastTime + "&n=0&network=1" //Development network
 #else
- "beacon/stream/0?since=1377357375&n=0"
+ "beacon/stream/0?since="+LastTime+"&n=0"
 #endif
-                ); //TODO: fix this hack
+                        ); //TODO: fix this hack
+                    Thread.Sleep(10000);
+                }
+            }));
+            t.Start();
         }
-
         private bool loadCallback(string result)
         {
             JToken token = JObject.Parse(result).SelectToken("data");
+            if (token == null)
+            {
+                return true;
+            }
             int count = (int)token.SelectToken("length");
             foreach (var t in token.SelectToken("timeline").Children())
             {
@@ -82,7 +101,16 @@ namespace SparklrWP
                   {
                       this.Items.Add(new ItemViewModel() { LineOne = (string)t.SelectToken("message") });
                   });
+                if (LastTime < (int)t.SelectToken("time"))
+                {
+                    LastTime = (int)t.SelectToken("time");
+                }
+                if (LastTime < (int)t.SelectToken("modified"))
+                {
+                    LastTime = (int)t.SelectToken("modified");
+                }
             }
+            isInLoadCycle = false;
             this.IsDataLoaded = true;
             return this.IsDataLoaded;
         }
