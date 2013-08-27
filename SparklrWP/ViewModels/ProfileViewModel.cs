c7081e50df74f9571a1ef72fc4a8ae272b64010c
ChrisK91 <ChrisGit@outlook.com>
2013-08-28 00:44:04 +0200

    Extended Profile page
    - Now using a view model (did set the properties programatically
    previously)
    - Introduced custom layout for the pivot control with bindable
    background image
    - Minor layout changes
    (references #26)

---
+++ SparklrWP/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SparklrWP
+{
+    public class ProfileViewModel : INotifyPropertyChanged
+    {
+        private string _handle;
+        private string _profileImage;
+        private string _backgroundImage;
+        private string _bio;
+        private int _id;
+
+        public string Handle
+        {
+            get
+            {
+                return _handle;
+            }
+            set
+            {
+                if (_handle != value)
+                {
+                    _handle = value;
+                    NotifyPropertyChanged("Handle");
+                }
+            }
+        }
+
+        public string BackgroundImage
+        {
+            get
+            {
+                return _backgroundImage;
+            }
+            set
+            {
+                if (_backgroundImage != value)
+                {
+                    _backgroundImage = value;
+                    NotifyPropertyChanged("BackgroundImage");
+                }
+            }
+        }
+
+        public string ProfileImage
+        {
+            get
+            {
+                return _profileImage;
+            }
+            set
+            {
+                if (_profileImage != value)
+                {
+                    _profileImage = value;
+                    NotifyPropertyChanged("ProfileImage");
+                }
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    NotifyPropertyChanged("ID");
+                }
+            }
+        }
+
+        public string Bio
+        {
+            get
+            {
+                return _bio;
+            }
+            set
+            {
+                if (_bio != value)
+                {
+                    _bio = value;
+                    NotifyPropertyChanged("Bio");
+                }
+            }
+        }
+
+        public ProfileViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
