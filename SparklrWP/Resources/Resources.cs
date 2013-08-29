573b9d981e363c25fe6f127bff88a3f4a11b7d88
Marocco2 <marocco2@live.it>
2013-08-29 15:19:33 +0200

    Add an localization file. TODO: Using!

---
+++ SparklrWP/Resources/Resources.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SparklrWP.Resources
+{
+    class Resources
+    {
+        private readonly static AppResource localizedresources = new AppResource();
+
+        public AppResource LocalizedResources
+        {
+            get
+            {
+                return localizedresources;
+            }
+        }
+    }
+}
