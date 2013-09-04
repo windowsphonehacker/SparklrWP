02866771e4b48fbda384a88a9034cb00bba7d177
Corné Oppelaar <he@eaterofcorps.es>
2013-09-04 17:34:10 +0200

    fix background

---
+++ SparklrWP/Controls/SparklrPostControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SparklrWP.Controls
+{
+    public partial class SparklrPostControl : UserControl
+    {
+        public SparklrPostControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
