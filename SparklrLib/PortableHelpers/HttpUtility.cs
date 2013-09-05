ddf0391e894d29170c447c0198401ba2ed05e452
jessenic <jessenic@digiex.net>
2013-09-05 02:28:34 -0700

    Fix building the portable lib

---
+++ SparklrLib/PortableHelpers/HttpUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#if PORTABLELIB
+namespace System.Net
+{
+    internal class HttpUtility
+    {
+        internal static string UrlEncode(string keyword)
+        {
+            return Uri.EscapeDataString(keyword);
+        }
+    }
+}
+#endif
