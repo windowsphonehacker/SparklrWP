58a6dcbd625c82f7548263e772877b0c228e06b0
Marocco2 <marocco2@live.it>
2014-04-07 15:08:31 +0200

    Added Bcl and Other good stuff

--- SparklrForWindowsPhone/SparklrForWindowsPhone/Properties/AssemblyInfo.cs
+++ SparklrForWindowsPhone/SparklrForWindowsPhone/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SparklrForWindowsPhone")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Sparklr 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
